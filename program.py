97ae8c6d58608908f1d57f81604f83a5c123c6a7
Lex Leo Gryfon <lexleogryfon@users.noreply.github.com>
2018-02-16 21:23:36 +0300

    returns read-only dictionary where key is '.extension' and value a tuple(of path objectes)
    print extensions sorted by number of files

--- program.py
+++ program.py
@@ -1,15 +1,15 @@
 #!/usr/bin/env nix-shell
 #!nix-shell -p python35 -i python3.5
 
-
+import sys
 from pathlib import Path
 from os import path as ospath
-import sys
+from types import MappingProxyType
 
 ### variables
-exts = set()    
-ext_dict = {}
-input=''
+#exts = set()    
+#ext_dict = {}
+#input=''
 
 def scan(path_string):
     p = Path(path_string)
@@ -22,32 +22,68 @@ def scan(path_string):
             # file = str(child)
             # extension = ospath.splitext(file)[-1].lower()
             extension = child.suffix.lower()
-
-
-            
-            exts.add(extension)
+            # exts.add(extension)
             if extension in ext_dict.keys():
                 ext_dict[extension] = ext_dict[extension] +1          
             else:
                 ext_dict[extension] = 1
 
 
-
+def scanv2(path_string, ext_dict_param={}):
+    '''
+    returns read-only dictionary where key is '.extension' and value a tuple(of path objectes)
+    '''
+    ext_dict = ext_dict_param
+    p = Path(path_string)
+    for child in p.iterdir():
+       
+        if child.is_dir():
+            scanv2(child, ext_dict_param=ext_dict)
+        else:
             
+            # file = str(child)
+            # extension = ospath.splitext(file)[-1].lower()
+            extension = child.suffix.lower()
+            # exts.add(extension)
+            if extension in ext_dict.keys():
+                #print(type(ext_dict[extension]), ext_dict[extension], ext_dict)
+                ext_dict[extension] +=  (child, )
+            else:
+                #print(type(child), child, dir(child))
+                ext_dict[extension] = (child, )
+
+    return MappingProxyType(ext_dict)
+    #return ext_dict
 
 
 def main(argv):
     # scan(input)
-    scan(argv[1])
-    #print(exts)
-    #print(ext_dict)
-    # for key,value in ext_dict.items(): print(value, key)
-    #print(ext_dict.get('.sample'))
-    #print(sorted(ext_dict, key=ext_dict.get, reverse=True))
-    s = [(key, ext_dict[key]) for key in sorted(ext_dict, key=ext_dict.get, reverse=True)]
-    for ext, count in s: print('{count} {ext} files'.format(ext=ext, count=count))
+    ext_dict = scanv2(argv[1])
+   
+    #block v1
+    #print extensions sorted by number of files
+    #var = []
+    #for extension, tuple_of_files in ext_dict.items():
+        #var.append((extension, len(tuple_of_files),))
+    ##^equals var = [(extension, len(tuple_of_files)) for extension, tuple_of_files in  ext_dict.items()]
+    #s = sorted(var, key=lambda tpl: tpl[1], reverse=True)
+    #for ext, count in s:
+        #print('{count} {ext} files'.format(ext=ext, count=count))
+    
+    #block v2
+    #print extensions sorted by number of files
+    for ext, count in sorted( [(extension, len(tuple_of_files)) for extension, tuple_of_files in  ext_dict.items()], 
+                              key=lambda tpl: tpl[1], 
+                              reverse=True): 
+        print('{count} {ext} files'.format(ext=ext, count=count))
+    
+  
+# old code 
+#    s = [(key, ext_dict[key]) for key in sorted(ext_dict, key=ext_dict.get, reverse=True)]
+#    for ext, count in s: print('{count} {ext} files'.format(ext=ext, count=count))
 
-    #TODO store list of filenames, paths in ext_dict, along with extension number occurence ; we may not really count number of extension occurence, but just count a lenth of file list with particular extension
+    #DONE store list of filenames, paths in ext_dict, along with extension number occurence ; we may not really count number of extension occurence, but just count a length of file list with particular extension
+    #DONE return immutable value in scan()
     #TODO convert singe file
     #TODO convert singe file and write it in relative path inside multiple directories
     #TODO implement blocking loop, and covert file after file
