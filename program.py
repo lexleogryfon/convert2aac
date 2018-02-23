425d57af0a445a02723693dc98965745674409e0
Lex Leo Gryfon <lexleogryfon@users.noreply.github.com>
2018-02-23 20:44:44 +0300

    MVP v0.01 mass conversion and copy works

--- program.py
+++ program.py
@@ -1,10 +1,16 @@
 #!/usr/bin/env nix-shell
 #!nix-shell -p python35 -i python3.5
 
-import sys, subprocess, os
+'''
+made in wingIde 6 pro
+lex(c)
+'''
+
+
+
 from pathlib import Path
-#from os import path as ospath
 from types import MappingProxyType
+import sys, subprocess, os, shutil
 
 
 def scan(path_string, ext_dict_param={}):
@@ -31,61 +37,81 @@ def scan(path_string, ext_dict_param={}):
 
 
 def convert_file(input_file, output_prefix_folder):
-    #call ffmpeg cli
-#    os.chdir("/home/lex/Desktop/mystuff/pc/media/media/music/Dubstep")
-    # output = subprocess.check_output('ffmpeg -i Bjork\ -\ Pagan\ Poetry\ \(Ripperton\ rmx\).mp3 -vn -c:a libfdk_aac -profile:a aac_he_v2 -b:a 32k -y output2.m4a', shell=True, stderr=subprocess.STDOUT, universal_newlines=True)
-    # print(output)
-    #subprocess.Popen(['pwd'])
-    #subprocess.Popen(['ls', '-la'])
+    '''
+    accept pathlib.Path objects as parameters
+    '''
+
     devnull = open('/dev/null', 'w')
-    print("BEFORE")
-    print(output_prefix_folder)
     output_prefix_folder = './' / output_prefix_folder / str(input_file.parent)[1:]
-    print(output_prefix_folder)
+    # print('new files will be saved in ', output_prefix_folder)
     
     output_prefix_folder.mkdir(exist_ok=True, parents=True)
     output_file_name = str(input_file.stem) + '.m4a'
-    output_file_path = output_prefix_folder / output_file_name  
+    output_file_path = output_prefix_folder / output_file_name
+    #spawn non blocking async background process
     p = subprocess.Popen(['/home/lex/.nix-profile/bin/ffmpeg', '-threads', '1', '-i', str(input_file), '-vn',   '-c:a', 'libfdk_aac', '-profile:a', 'aac_he_v2', '-b:a', '32k', '-y', str(output_file_path)], stdout=devnull, stderr=devnull)
-    print(p, p.returncode, p.poll() )
-    print("DO NEW TASK")
-    while p.poll() is None:
-        import time
-        time.sleep(0.1)
-    print(p.poll())
+
+    # blocking wait until completion, to get exit code
+    #while p.poll() is None:
+        #import time
+        #time.sleep(0.1)
+    #print(p.poll())
+#    if p.returncode != 0: raise CalledProcessError(input_file, p, p.returncode )    
+
+
+def copy_file(input_file, output_prefix_folder):
+    '''
+    accept pathlib.Path objects as parameters
+    '''
+    output_prefix_folder = './' / output_prefix_folder / str(input_file.parent)[1:]
+    # print('new files will be saved in ', output_prefix_folder)
+    
+    output_prefix_folder.mkdir(exist_ok=True, parents=True)
+    #copy_file
+    shutil.copy2(src=str(input_file), dst=str(output_prefix_folder))
+
+
 
 def main(argv):
     files = scan(argv[1])
-    #v 1 print extensions sorted by number of files
+    #print extensions sorted by number of files
     for ext, count in sorted( [(extension, len(tuple_of_files)) for extension, tuple_of_files in  files.items()], 
                               key=lambda tpl: tpl[1], 
                               reverse=True): 
         print('{count} {ext} files'.format(ext=ext, count=count))
 
-    #v 2 through map #does't work and unnecessary
-    #print([*map(lambda ext, count: (ext, count), sorted( [(extension, len(tuple_of_files)) for extension, tuple_of_files in  files.items()], 
-                              #key=lambda tpl: tpl[1], 
-                              #reverse=True))])
-                              
-    #v 3 through list comprehension
-    #[print('{count} {ext} files'.format(ext=ext, count=count)) for ext, count in sorted( [(extension, len(tuple_of_files)) for extension, tuple_of_files in  files.items()], 
-                              #key=lambda tpl: tpl[1], 
-                              #reverse=True)]
 
-    i_f = Path('/home/lex/Desktop/mystuff/pc/media/media/music/Dubstep/Bjork - Pagan Poetry (Ripperton rmx).mp3')
-    print(i_f.exists())
-    #create output folder
-    o_p_f = Path('./subfolder/ыги')
-    convert_file(input_file=i_f, output_prefix_folder=o_p_f)
+
+    # single file conversion example
+    #i_f = Path('/home/lex/Desktop/mystuff/pc/media/media/music/Dubstep/Bjork - Pagan Poetry (Ripperton rmx).mp3')
+    #o_p_f = Path('./subfolder/')
+    #convert_file(input_file=i_f, output_prefix_folder=o_p_f)
+
+    ALLOWED_EXTENSIONS = ('.mp3',)
+    o_p_f = Path('./subfolder/')
+    
+    for key,values in files.items():
+        print('key: ', key)
+        for item in values:
+            print(item)
+            if key in ALLOWED_EXTENSIONS:
+                convert_file(input_file=item, output_prefix_folder=o_p_f)
+            else:
+                copy_file(input_file=item, output_prefix_folder=o_p_f)
+    
 
     #DONE store list of filenames, paths in ext_dict, along with extension number occurence ; we may not really count number of extension occurence, but just count a length of file list with particular extension
     #DONE return immutable value in scan()
+    #DONE convert singe file
+    #DONE convert singe file and write it in relative path inside multiple directories
+
+    #DONE implement blocking loop, and covert file after file
+    #TODO add logging for python app itself, for ffmpeg & if ffmpeg exit code != 0
+    #DONE allow conversion only by specific extension
+    #DONE copy non convertable files
+    #TODO convert multiple files at same time    
     #TODO add argparse CLI parameters
-    #TODO convert singe file
-    #TODO convert singe file and write it in relative path inside multiple directories
-    #TODO implement blocking loop, and covert file after file
-    #TODO allow conversion only by specific extension
-    #TODO convert multiple files at same time
+
 
 
 
