6935df4727c66a2b5689eee38b7ca65b5d37fc79
Lex Leo Gryfon <lexleogryfon@users.noreply.github.com>
2018-03-01 15:50:34 +0300

    major improvements added logging added CLI args

--- program.py
+++ program.py
@@ -10,7 +10,10 @@ lex(c)
 
 from pathlib import Path
 from types import MappingProxyType
-import sys, subprocess, os, shutil
+import sys, subprocess, os, shutil, argparse, time, logging
+
+pool = []
+logging.basicConfig(filename='convert.log')
 
 
 def scan(path_string, ext_dict_param={}):
@@ -49,15 +52,33 @@ def convert_file(input_file, output_prefix_folder):
     output_file_name = str(input_file.stem) + '.m4a'
     output_file_path = output_prefix_folder / output_file_name
     #spawn non blocking async background process
-    p = subprocess.Popen(['/home/lex/.nix-profile/bin/ffmpeg', '-threads', '1', '-i', str(input_file), '-vn',   '-c:a', 'libfdk_aac', '-profile:a', 'aac_he_v2', '-b:a', '32k', '-y', str(output_file_path)], stdout=devnull, stderr=devnull)
+    p = subprocess.Popen(['/home/lex/.nix-profile/bin/ffmpeg', '-threads', '1', '-i', str(input_file), '-vn',   '-c:a', 'libfdk_aac', '-profile:a', 'aac_he_v2', '-b:a', '32k', '-y', str(output_file_path)], universal_newlines=True, stdout=subprocess.PIPE , stderr=subprocess.PIPE)
 
     # blocking wait until completion, to get exit code
     #while p.poll() is None:
         #import time
         #time.sleep(0.1)
     #print(p.poll())
-#    if p.returncode != 0: raise CalledProcessError(input_file, p, p.returncode )    
-
+#    if p.returncode != 0: raise CalledProcessError(input_file, p, p.returncode )
+    global pool
+    if len(pool) < 7:
+        pool.append(p)
+    
+    while len(pool) >= 6:
+        for task in pool:
+            if task.poll() is None:
+                time.sleep(0.1)
+            elif task.returncode is 0:
+                pool.remove(task)
+            else:
+                outs, errs = task.communicate()
+                err = str(task.returncode) + ' '.join(task.args) + outs + errs
+                logging.error(msg=err)
+                pool.remove(task)
+                #log exit code of task with task args
+                #remove task from pool
+            
+    
 
 def copy_file(input_file, output_prefix_folder):
     '''
@@ -71,14 +92,34 @@ def copy_file(input_file, output_prefix_folder):
     shutil.copy2(src=str(input_file), dst=str(output_prefix_folder))
 
 
+def queue(task):
+    '''accept function as argument'''
+    
+    
+
+    pass
+
 
 def main(argv):
-    files = scan(argv[1])
-    #print extensions sorted by number of files
-    for ext, count in sorted( [(extension, len(tuple_of_files)) for extension, tuple_of_files in  files.items()], 
+
+    
+    if len(argv) is 1:
+        argv.append("-h")   
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-s", "--scan", help="show extensions sorted by number of files")
+    parser.add_argument("-i", "--input", help="input folder")
+    parser.add_argument("-o", "--output", help="output folder")
+    arguments = parser.parse_args()
+
+    if arguments.scan:
+        
+    
+        files = scan(arguments.scan)
+        #print extensions sorted by number of files
+        for ext, count in sorted( [(extension, len(tuple_of_files)) for extension, tuple_of_files in  files.items()], 
                               key=lambda tpl: tpl[1], 
-                              reverse=True): 
-        print('{count} {ext} files'.format(ext=ext, count=count))
+                              reverse=True):
+            print('{count} {ext} files'.format(ext=ext, count=count))
 
 
 
@@ -87,30 +128,37 @@ def main(argv):
     #o_p_f = Path('./subfolder/')
     #convert_file(input_file=i_f, output_prefix_folder=o_p_f)
 
-    ALLOWED_EXTENSIONS = ('.mp3',)
-    o_p_f = Path('./subfolder/')
+    if arguments.input and arguments.output:
+        print(arguments.input, arguments.output)
+        
+        ALLOWED_EXTENSIONS = ('.mp3','.wav','.ogg', '.wma','.flac')
+        #o_p_f = Path('./subfolder/')
+        files = scan(arguments.input)
+        o_p_f = Path(arguments.output)
+        
     
-    for key,values in files.items():
-        print('key: ', key)
-        for item in values:
-            print(item)
-            if key in ALLOWED_EXTENSIONS:
-                convert_file(input_file=item, output_prefix_folder=o_p_f)
-            else:
-                copy_file(input_file=item, output_prefix_folder=o_p_f)
-    
+        for key,values in files.items():
+            #print('key: ', key)
+            for item in values:
+                # print(item)
+                if key in ALLOWED_EXTENSIONS:
+                    convert_file(input_file=item, output_prefix_folder=o_p_f)
+                else:
+                    copy_file(input_file=item, output_prefix_folder=o_p_f)
+
+
 
     #DONE store list of filenames, paths in ext_dict, along with extension number occurence ; we may not really count number of extension occurence, but just count a length of file list with particular extension
     #DONE return immutable value in scan()
     #DONE convert singe file
     #DONE convert singe file and write it in relative path inside multiple directories
-
     #DONE implement blocking loop, and covert file after file
-    #TODO add logging for python app itself, for ffmpeg & if ffmpeg exit code != 0
+    #DONE convert multiple files at same time    
     #DONE allow conversion only by specific extension
     #DONE copy non convertable files
-    #TODO convert multiple files at same time    
-    #TODO add argparse CLI parameters
+
+    #DONE add argparse CLI parameters
+    #DONE add logging for python app itself, for ffmpeg & if ffmpeg exit code != 0
 
 
 
