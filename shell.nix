766e16d70a1a8be280d3036c0ec910a1bfab4ece
Lex Leo Gryfon <lexleogryfon@users.noreply.github.com>
2018-03-12 00:01:19 +0300

    unicode ascii exception bug solved

--- shell.nix
+++ shell.nix
@@ -1,5 +1,6 @@
 with import <nixpkgs> { };
 let
   ffmpeg-with-fdkaac = ffmpeg-full.override { nonfreeLicensing = true; fdkaacExtlib = true; };
+  
 in
-  runCommand "dummy" { buildInputs = [ python35Full ffmpeg-with-fdkaac ]; } ""
+  runCommand "dummy" { buildInputs = [ python35Full ffmpeg-with-fdkaac glibc glibcLocales]; LANG = "en_US.UTF-8"; } ""
